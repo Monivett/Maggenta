507155c79f85a001391e8eefbf7c3ceb2ad245aa
Monivett <monichapa1@hotmail.com>
2022-05-18 19:06:32 -0500

    Compartir button

--- Maggenta/frontend/src/Paginas/Publicacion.js
+++ Maggenta/frontend/src/Paginas/Publicacion.js
@@ -173,20 +173,27 @@ function Publicacion() {
           </div>
           <div className="col">
 
-           {user && <div className=" m-2  " >
-              <div className="">
-                {isLiked ? <button className="btn btn-outline-info-danger m-2" type="button" onClick={e => UnLike(user.userData._id, publicaciones._id)}>
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heartbreak-fill" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M8.931.586 7 3l1.5 4-2 3L8 15C22.534 5.396 13.757-2.21 8.931.586ZM7.358.77 5.5 3 7 7l-1.5 3 1.815 4.537C-6.533 4.96 2.685-2.467 7.358.77Z" />
-                  </svg>
-                </button>
-                  : <button className="btn btn-outline-info m-2 " type="button" onClick={e => Like(user.userData._id, publicaciones._id)}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-heart-fill" viewBox="0 0 16 16">
-                      <path fillRule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" />
+            {user &&
+              <div className=" m-2  " >
+                <div className="">
+                  {isLiked ? <button className="btn btn-outline-info-danger m-2" type="button" onClick={e => UnLike(user.userData._id, publicaciones._id)}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heartbreak-fill" viewBox="0 0 16 16">
+                      <path fill-rule="evenodd" d="M8.931.586 7 3l1.5 4-2 3L8 15C22.534 5.396 13.757-2.21 8.931.586ZM7.358.77 5.5 3 7 7l-1.5 3 1.815 4.537C-6.533 4.96 2.685-2.467 7.358.77Z" />
                     </svg>
-                  </button>}
+                  </button>
+                    : <button className="btn btn-outline-info m-2 " type="button" onClick={e => Like(user.userData._id, publicaciones._id)}>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-heart-fill" viewBox="0 0 16 16">
+                        <path fillRule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" />
+                      </svg>
+                    </button>}
+                </div>
+                <div className="">
+                  <button className="btn btn-outline-info m-2 " type="button">
+                    Compartir imagen
+                  </button>
+                </div>
               </div>
-            </div>}
+            }
             {/** FOR DE COMENTARIOS */}
             {coment.map(ElComentario => (
               <div className="card  m-2  " >
