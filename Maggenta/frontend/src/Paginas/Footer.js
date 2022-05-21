9a6cacafa85841a9abd4dcdc517cca2a24268bd4
Monivett <monichapa1@hotmail.com>
2022-05-20 23:53:42 -0500

    Footer

--- Maggenta/frontend/src/Paginas/Footer.js
+++ Maggenta/frontend/src/Paginas/Footer.js
@@ -1,25 +1,38 @@
-import { Fragment } from "react";
+import { useState, useCallback, useEffect, Fragment } from "react";
+import { Getall } from "../services//CategoryService";
 import './Colores.css';
 
 function Foot() {
+
+    const [categorias, setCategorias] = useState([]);
+
+    const getCategorias = useCallback(async () => {
+
+        const categorias = await Getall();
+
+        setCategorias(categorias);
+
+    }, [])
+
+    useEffect(() => {
+
+        getCategorias();
+
+    }, [getCategorias]);
+
     return (
         <Fragment>
             <footer className="bg3 text-white text-center " >
                 <label htmlFor="inputAddress " className="m-5">Categorias:</label>
                 <div className="container-fluid " >
                     <nav className="row ">
-                        <ul className="col-4 mt-2 list-unstyled">
-                            <button type="button" className="btn btn-dark mt-2 mb-2">FanArts</button><br />
-                            <button type="button" className="btn btn-dark mt-2 mb-2">Escenarios</button><br />
-                        </ul>
-                        <ul className="col-4 mt-2 list-unstyled">
-                            <button type="button" className="btn btn-dark mt-2 mb-2">Sketch</button><br />
-                            <button type="button" className="btn btn-dark mt-2 mb-2">Digital</button><br />
-                        </ul>
-                        <ul className="col-4 mt-2 list-unstyled">
-                            <button type="button" className="btn btn-dark mt-2 mb-2">Tradicional</button><br />
-                            <button type="button" className="btn btn-dark mt-2 mb-2">Inicio</button><br />
-                        </ul>
+                        {categorias.map((category) => (
+
+                            <ul className="col-4 mt-2 list-unstyled">
+                                <button type="button" className="btn btn-dark mt-2 mb-2">{category.Category}</button><br />
+                            </ul>
+
+                        ))}
                     </nav>
                 </div>
             </footer>
